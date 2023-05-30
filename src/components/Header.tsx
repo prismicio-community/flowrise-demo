ac744ab80edb899a77324c8eaa189adab9e1f196
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-05-30 17:49:37 -0400

    Tweak heading styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,42 @@
 import { createClient } from "../../prismicio";
 import Bounded from "@/components/Bounded";
 import Logo from "@/components/Logo";
+import { LinkField } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import { KeyTextField } from "@prismicio/types";
 
 export default async function Header() {
   const client = createClient();
 
   const settings = await client.getSingle("settings");
+  await console.log(
+    "🚀 ~ file: Header.tsx:9 ~ Header ~ settings:",
+    settings.data.nav_menu
+  );
 
   return (
-    <Bounded as="header">
-      <div className="flex justify-between">
+    <Bounded as="header" className=" py-4 md:py-6 lg:py-8">
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
         <Logo />
-        <nav>Navigation</nav>
+        <nav>
+          <ul className="flex text-base">
+            {settings.data.nav_menu.map(
+              ({
+                link,
+                link_text,
+              }: {
+                link: LinkField;
+                link_text: KeyTextField;
+              }) => (
+                <ul key={link_text}>
+                  <PrismicNextLink className="py-3 px-3" field={link}>
+                    {link_text}
+                  </PrismicNextLink>
+                </ul>
+              )
+            )}
+          </ul>
+        </nav>
       </div>
     </Bounded>
   );
