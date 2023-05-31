03a963e8908cf19747b4d105cab595d095b83a33
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-05-31 08:23:44 -0400

    Add generic page file

---
+++ src/app/[uid]/page.tsx
@@ -0,0 +1,15 @@
+import { SliceZone } from "@prismicio/react";
+import { components } from "../../../slices";
+import { createClient } from "../../../prismicio";
+
+export default async function Page({ params }: { params: { uid: string } }) {
+  const client = createClient();
+
+  const page = await client.getByUID("page", params.uid);
+
+  return (
+    <main>
+      <SliceZone components={components} slices={page.data.slices} />
+    </main>
+  );
+}
