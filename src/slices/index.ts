6e67257a056f360d8ed58ef6105493a61a4f165b
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-06-26 11:08:23 -0400

    Fix testimonials import

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -6,9 +6,6 @@ export const components = {
   call_to_action: dynamic(() => import("./CallToAction")),
   features: dynamic(() => import("./Features")),
   hero: dynamic(() => import("./Hero")),
-  testimonials: dynamic(
-    // @ts-expect-error Async Server Component
-    () => import("./Testimonials")
-  ),
+  testimonials: dynamic(() => import("./Testimonials")),
   text_with_image: dynamic(() => import("./TextWithImage")),
 };
