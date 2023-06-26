dbc75b93ad501cbee65532723c283dbe5cf81a67
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-06-26 17:38:59 -0400

    Remove redundant style

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -16,7 +16,7 @@ export default function Heading({
   return (
     <Comp
       className={clsx(
-        "font-bold leading-tight tracking-tight md:leading-tight font-display text-slate-700",
+        "font-bold leading-tight tracking-tight font-display text-slate-700",
         size === "xl" && "text-5xl md:text-7xl",
         size === "lg" && "text-4xl md:text-5xl",
         size === "md" && "text-3xl md:text-4xl",
