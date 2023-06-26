9399b770254f7376bb43e797dfd18df2e1ac677e
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-06-26 11:08:16 -0400

    Add generateMetadata to pages

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,7 +1,38 @@
+import { Metadata, ResolvingMetadata } from "next";
+import * as prismic from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { components } from "@/slices";
 import { createClient } from "@/prismicio";
+
+type Props = {
+  params: { uid: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const uid = params.uid;
+
+  // fetch data
+  const client = createClient();
+
+  const page = await client.getByUID("page", uid);
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: page.data.meta_title,
+    description: prismic.asText(page.data.meta_description),
+    openGraph: {
+      images: [page.data.meta_image.url || ""],
+    },
+  };
+}
 
 export default async function Page({ params }: { params: { uid: string } }) {
   const client = createClient();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import { Metadata, ResolvingMetadata } from "next";
+import * as prismic from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { components } from "@/slices";
 import { createClient } from "@/prismicio";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // fetch data
+  const client = createClient();
+
+  const page = await client.getSingle("homepage");
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: page.data.meta_title,
+    description: prismic.asText(page.data.meta_description),
+    openGraph: {
+      images: [page.data.meta_image.url || ""],
+    },
+  };
+}
 
 export default async function Home() {
   const client = createClient();
