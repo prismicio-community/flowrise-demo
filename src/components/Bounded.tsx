12e55b08fd878e2d47f4fbd8eaecee4d17d94c54
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-07-17 17:27:04 -0400

    Allow for props to be spread on Bounded

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -10,9 +10,13 @@ export default function Bounded({
   as: Comp = "section",
   className,
   children,
+  ...restProps
 }: BoundedProps) {
   return (
-    <Comp className={clsx("px-4 py-10 md:py-14 md:px-6 lg:py-16", className)}>
+    <Comp
+      className={clsx("px-4 py-10 md:py-14 md:px-6 lg:py-16", className)}
+      {...restProps}
+    >
       <div className={"mx-auto w-full max-w-6xl"}>{children}</div>
     </Comp>
   );
